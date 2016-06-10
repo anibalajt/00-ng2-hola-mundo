1020c51128270f947fb8a87890cade0216796053
Alberto Basalo <albertobasalo@hotmail.com>
2016-06-10 10:54:40 +0200

    ng init

---
+++ src/app/hola-mundo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'hola-mundo-app',
+  templateUrl: 'hola-mundo.component.html',
+  styleUrls: ['hola-mundo.component.css']
+})
+export class HolaMundoAppComponent {
+  title = 'hola-mundo works!';
+}
